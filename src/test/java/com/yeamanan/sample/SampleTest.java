ea844e632971e4f0aabc33ce45c158574e04cbbc
patilume <patilume@gmail.com>
2019-07-11 18:11:49 +0530

    Update SampleTest.java

--- src/test/java/com/yeamanan/sample/SampleTest.java
+++ src/test/java/com/yeamanan/sample/SampleTest.java
@@ -2,6 +2,20 @@ package com.yeamanan.sample;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+/**
+     * Holds an instance of the class we are testing.
+     */
+    private Sample smpl;
+
+    /**
+     * JUnit set up method.
+     */
+    @Before
+    public final void setUp() {
+        smpl = new Sample();
+    }
+
+
 
 /**
  * SampleTest class.
@@ -15,7 +29,7 @@ public class SampleTest {
      */
     @Test
     public final void test() {
-        assertEquals("This is ok", Sample.write(), "This is a sample project");
+        assertEquals("This is ok", smpl.write(), "This is a sample project");
         //assertEquals(new Sample.write(), "This is a sample project");
     }
 }
