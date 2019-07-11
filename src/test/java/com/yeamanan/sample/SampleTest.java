7f6c9f4a555d5962d787a276bae9cd0604aec56b
patilume <patilume@gmail.com>
2019-07-11 13:34:22 +0530

    Update SampleTest.java

--- src/test/java/com/yeamanan/sample/SampleTest.java
+++ src/test/java/com/yeamanan/sample/SampleTest.java
@@ -16,6 +16,6 @@ public class SampleTest {
      */
     @Test
     public final void test() {
-        assertEquals("This is ok", Sample.write(), "This is a sample project");
+        assertEquals("This is ok", sample.write(), "This is a sample project");
     }
 }
