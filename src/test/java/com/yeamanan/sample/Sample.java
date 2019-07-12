267715d564b28894362a0fc42f44723c49eae536
patilume <patilume@gmail.com>
2019-07-12 23:25:50 +0530

    Create Sample.java

---
+++ src/test/java/com/yeamanan/sample/Sample.java
@@ -0,0 +1,35 @@
+package com.yeamanan.sample;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Sample class.
+ *
+ * @author Yeam Anan <yeamanan@gmail.com>
+ */
+public class Sample {
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOGGER = Logger.getLogger(Sample.class);
+
+    /**
+     * Main() method.
+     *
+     * @param args arguments
+     */
+    public static void main(final String[] args) {
+        LOGGER.info(write());
+    }
+
+    /**
+     * write() method.
+     *
+     * @return some text
+     */
+    public static String write() {
+    //public String write() {
+        return "This is a sample project";
+    }
+}
